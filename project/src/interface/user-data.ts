b25e622e5469a7979c8ae39494044bc551783f54
Adrian Majcher <majcherrr87@gmail.com>
2022-12-08 11:03:26 +0100

    controller

---
+++ project/src/interface/user-data.ts
@@ -0,0 +1,5 @@
+export interface UserData {
+    surname: string;
+    name: string;
+    age: number
+}
---
+++ project/src/utils/sleep.ts
@@ -0,0 +1,3 @@
+export function sleep(){
+    return new Promise(resolve => setTimeout(() =>resolve('Hello, World'),2500));
+}
